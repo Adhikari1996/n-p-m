17624040dfb3d1d1357e3f2db50f83b68d981872
rajadhikari1996 <tech.codetechyons>
2021-02-13 07:36:42 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// const _ = require('lodash');
+
+const numbers = [12, 53, 53, 56, 33];
+// _.each(numbers, function (number, i) {
+//   console.log(number);
+// });
+numbers.forEach(number => console.log(number));
